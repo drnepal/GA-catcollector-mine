b943243d76fec417570ff0126bb99118a4a61a8d
devrajn <devraj.nepal@gmail.com>
2023-03-02 13:31:31 -0600

    cats index using db data instead of dummy data

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-
-from .mo
+# Import the Cat Model
+from .models import Cat
 # temporary cats for building templates
-cats = [
-    {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-    {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-    {'name': 'Tubs', 'breed': 'ragdoll', 'description': 'chunky lil guy', 'age': 0},
-]
+# cats = [
+#     {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#     {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+#     {'name': 'Tubs', 'breed': 'ragdoll', 'description': 'chunky lil guy', 'age': 0},
+# ]
 
 # Create your views here.
 # view functions match urls to code (like controllers in Express)
@@ -20,4 +20,4 @@ def about(request):
 def cats_index(request):
     # just like we passed data to our templates in express
     # we pass data to our templates through our view functions
-    return render(request, 'cats/index.html', { 'cats': cats })
+     return render(request, 'cats/index.html', { 'cats': cats })'cats': cats })
